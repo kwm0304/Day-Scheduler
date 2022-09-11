a538f49ab86d9f24da2c9fcaaf5149dd8886d528
Kenan <mckenzie.kenan@yahoo.com>
2022-09-11 16:49:03 -0400

    linked save btn to the time and input, added localStorage

--- assets/script.js
+++ assets/script.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
     var currentDate = moment();
 
     currentDay.textContent = currentDate.format("MMM DD, YYYY h:mm a");
+
+    $('.saveBtn').on('click', function() {
+        var input = $(this).siblings('description').val();
+        var time = $(this).parent().attr('id');
+
+        localStorage.setItem(time, input);
+    })
 }) 
 
