b3d608c1801531d9d44b142c68a44f5eaf2f73d0
Kenan <mckenzie.kenan@yahoo.com>
2022-09-11 20:59:41 -0400

    added getItem to retrieve from localStorage

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,26 @@ $(document).ready(function(){
     currentDay.textContent = currentDate.format("MMM DD, YYYY h:mm a");
 
     $('.saveBtn').on('click', function() {
-        var input = $(this).siblings('description').val();
-        var time = $(this).parent().attr('id');
-
+        var input = $(this)
+            .siblings('.description')
+            .val();
+        var time = $(this)
+            .parent()
+            .attr('id');
+        //sends to local storage
         localStorage.setItem(time, input);
-    })
+    });
+
+    //loads from local storage. Will figure out how to use .each for this later
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    
 }) 
 
